2aa07b82d6dc04e46d924125709382f5b6087c0f
Cesare Domizi <domizicesare@gmail.com>
2023-01-11 16:14:07 +0100

    add simple button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
+      <Button
+        variant="contained"
+        onClick={() => console.log("test")}
+      >
+        Hello World</Button>
     </div>
   );
 }
