b5ab167bde26cc4537ece5a06be05166874669ae
Cesare Domizi <domizicesare@gmail.com>
2023-02-20 19:17:51 +0100

    add item delete button

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -1,13 +1,41 @@
-import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
+// mui components
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  Tooltip
+} from "@mui/material";
+
+// mui icons
 import CircleIcon from '@mui/icons-material/Circle';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function TodoItem(props) {
+  
+  const onDeleteTodo = () => {
+    console.log('delete item')
+  };
+
   return (
     <ListItem>
       <ListItemIcon>
         <CircleIcon fontSize="small" />
       </ListItemIcon>
-      <ListItemText primary={props.name} />
+      <ListItemText
+        primary={props.name}
+        sx={{ flexGrow: 0 }}
+      />
+      <Tooltip title="Delete">
+        <IconButton
+          aria-label="delete"
+          size="small"
+          sx={{ ml: 2}}
+          onClick={onDeleteTodo}
+        >
+          <DeleteIcon fontSize="small" />
+        </IconButton>
+      </Tooltip>
     </ListItem>
   );
 }
