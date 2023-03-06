d35380b34415f1ceb71b2748e186a11715502516
Cesare Domizi <domizicesare@gmail.com>
2023-03-06 21:54:25 +0100

    add cards to homepage

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,5 +1,20 @@
-const ContentCard = () => {
-  return <></>;
+// mui components
+import { capitalize, Card, CardActionArea, Typography } from "@mui/material";
+
+// mui icons
+import { ArrowOutward as ArrowOutwardIcon } from "@mui/icons-material";
+
+const ContentCard = ({ title, url, icon }) => {
+  const Icon = icon;
+  const itemIcon = icon ? <Icon sx={{ fontSize: "10rem" }} /> : false;
+  return (
+    <Card sx={{ minWidth: 275, textAlign: "center" }}>
+      <CardActionArea href={url}>
+        {itemIcon || <ArrowOutwardIcon></ArrowOutwardIcon>}
+        <Typography variant="h3">{capitalize(title)}</Typography>
+      </CardActionArea>
+    </Card>
+  );
 };
 
 export default ContentCard;
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -30,7 +30,7 @@ function RootLayout() {
         onToggle={toggleDrawer}
         menuItems={menuItems}
       />
-      <Box component="main" sx={{ mt: 8, p: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, mt: 8, p: 3 }}>
         <Outlet />
       </Box>
     </Box>
