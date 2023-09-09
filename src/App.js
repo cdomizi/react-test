cad53b53a0b8103f3e5ada0ab2728e6e2b1ab697
Cesare Domizi <domizicesare@gmail.com>
2023-09-09 14:02:19 +0200

    minor change: function declaration to expression

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Routes from "./routes";
 // MUI components
 import { Box } from "@mui/material";
 
-function App() {
+const App = () => {
   return (
     <Box className="App">
       <ThemeCustomization>
